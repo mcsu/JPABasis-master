0f3bb6019249634e3a89defc691bf560d2a4d7a9
charroux <benoit.charroux@free.fr>
2015-10-23 14:52:34 +0200

    first  commit

--- src/main/java/test/com/univ/App.java
+++ src/main/java/test/com/univ/App.java
@@ -11,10 +11,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import com.univ.Car;
 import com.univ.Person;
-import com.univ.Rent;
-import com.univ.Van;
 
 /**
  * Hello world!
@@ -24,73 +21,25 @@ public class App
 {
     public static void main( String[] args )
     {
-    	// manager1 voir fichier persistence.xml
     	EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager1");
 		EntityManager entityManager = emf.createEntityManager();
 		
 		EntityTransaction tx = entityManager.getTransaction();
 		
-			
-		tx.begin();
-		
-		Car car = new Car();
-		car.setPlateNumber("AA 11 BB");	// clef primaire
-		car.setNumberOfSeats(5);
-		
-		Van van = new Van();			
-		van.setMaxWeight(1000);
-		van.setPlateNumber("CC 22 DD");	// clef primaire
-		
-		try{
-			entityManager.persist(van);
-			tx.commit();
-			
-			Van van1 = entityManager.find(Van.class, "CC 22 DD");
-			
-			tx.begin();
-			entityManager.persist(car);
-			tx.commit();
-		}catch(Exception e){
-				// ici si clef primaire existe déjà
-			try{
-				tx.rollback();	// annule les persit
-			}catch(Exception e1){
-				// recapture l'excdeption générée par le rollback
-			}
-		}
-				
-		try{
-			tx = entityManager.getTransaction();
+    	try{
+    		
 			tx.begin();
 			
-			
-			Person person = new Person();
-			person.setName("Tintin");
-			
-			entityManager.persist(person);
-			
-			Rent rent = new Rent();
-			
+			Person p = new Person();
+			p.setName("Tintin");
+					
 			DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 			Date beginDate = dateFormat.parse("23/09/2015");
-			rent.setBeginRent(beginDate);
 			
-			Date endDate = dateFormat.parse("25/09/2015");
-			rent.setEndRent(endDate);
-	
-			entityManager.persist(rent);
-			
-			List<Rent> rents = person.getRents();
-			rents.add(rent);	// mise  à jour du côté many de l'association
-			
-			rent.setPerson(person);	// mise à jour du côté one de l'association
+			entityManager.persist(p);
 				
-			tx.commit();
-			
-			List<Person> liste = entityManager.createQuery("select p from Person p where p.name like 'Tintin'").getResultList();
-			for(Person p : liste){
-				System.out.println("person = " + p);
-			}
+			tx.commit();			
+		
 		}catch(Exception e){
 			tx.rollback();
 		}
